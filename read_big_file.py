7fbcd6936ddbe957c717ad159ba0ab4852dc4b17
matteushc <matteus_h@hotmail.com>
2018-12-16 19:43:30 -0200

    Update read_big_file.py

--- read_big_file.py
+++ read_big_file.py
@@ -1,10 +1,6 @@
 
 #Codigo para ler um arquivo grande e agrupar os numeros distintos e depois contar - Desafio
 
-#Primeiro iria ler o arquivo linha por linha sem carregar na memória. Durante a leitura iria registrar o id do cliente
-#em um conjunto (set), pois guarda somente os numeros únicos,  e depois iria contar a quantidade de registros acumulados 
-#nesse conjunto.
-
 from bitarray import bitarray
 
 count_user_id = bitarray(1000000000)
